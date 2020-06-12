022ef6659ef3fbffcb3ba77cca0660901a3e7800
Diego Saldano <dsaldano@preminens.com.ar>
2020-06-11 23:42:57 -0300

    fix conección a base de datos

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
-const Sequelize = require('sequelize');
+    const Sequelize = require('sequelize');
 
-//Option1 parametros separately
-module.exports = new Sequelize('palailadb', 'root', 'root', {
+//Option1 parametros separaso
+module.exports = new Sequelize('muebleapp', 'root', 'root', {
     host: '127.0.0.1',
     port: '3306',
     dialect: 'mysql',
@@ -14,15 +14,5 @@ module.exports = new Sequelize('palailadb', 'root', 'root', {
         acquire: 30000,
         idle: 10000
     },
-    operatorsAliases: false
+   operatorsAliases: false
 });
-
-//Option2 Pasar la conección por una URI
-//const sequelize = new Sequelize('URLPOSTGRES')
-
-/**SI uso sqlite3 paso de este modo la db */
-/* const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'path/to/database.sqlite'
-  });
- */
